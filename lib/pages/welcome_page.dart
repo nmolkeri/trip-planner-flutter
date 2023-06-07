c22e4a2fcee03bd7f2eef3b532d0e64226314b15
nmolkeri <nmolkeri@gmail.com>
2023-06-07 17:45:40 -0400

    added text design

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trip_finder/widgets/app_large_text.dart';
+import 'package:trip_finder/widgets/app_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -31,6 +33,29 @@ class _WelcomePageState extends State<WelcomePage> {
               fit: BoxFit.fill
             )
           ),
+          child: Container(
+            margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 8,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trip"),
+                      AppText(text: "Mountain", size: 30,),
+                      const SizedBox(height: 20),
+                      AppText(text: "Mountain hikes give you an incredible sense of freedom along with endurance tests")
+                    ],
+                  ),
+                ),
+                Expanded(flex: 2, 
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [AppText(text: "ss")],
+                ))
+            ],),
+          )
         );
       }),
     );
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  double size;
+  final Color color;
+  final String text;
+
+  AppText({super.key, 
+  required this.text,
+  this.color = Colors.black,
+  this.size = 16});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: size
+      )
+    );
+  }
+}
